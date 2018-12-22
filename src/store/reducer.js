d77788e6f3cbde6aaac9ed791551e9eaf4f4261a
Gabester0 <gceipper@gmail.com>
2018-12-22 11:17:07 -0500

    and another one

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,19 +7,28 @@ const reducer = (state = initialState, action)=>{
     switch(action.type){
         case 'INCREMENT':
         return {
+            ...state,
             counter : state.counter + 1
         }
         case  'DECREMENT':
         return {
+            ...state,
             counter : state.counter -1
         }
         case 'ADD':
         return {
-                 counter : state.counter + action.val
+            ...state,
+            counter : state.counter + action.val
              }
         case  'SUB':
         return {
+            ...state,
             counter : state.counter - action.val
+        }
+        case 'STORE_RESULT':
+        return {
+            ...state,
+            results : state.results.concat({id: new Date(), value: state.counter})
         }
     }
     return state;
